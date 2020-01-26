d17c89cbe037dd1a71323624f33383c1969c5320
Ashutosh <ashutosh@localhost.localdomain>
2020-01-26 10:14:32 +0530

    new commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	$('button').click(function(){
+		const name= $('#nameid').val();
+		$('.imgchange').attr('src',`https://joeschmoe.io/
+			api/v1/${name}`);  
+	});
+});
